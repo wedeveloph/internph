8b837b74edec5cf13866b42ae5a8929d6641adf3
Chia <chiamisola@gmail.com>
2020-01-06 22:13:23 -0500

    lllol

--- public/main.js
+++ public/main.js
@@ -5,63 +5,84 @@ var sheet = "https://spreadsheets.google.com/feeds/list/1073N87suMlax63_94Jip5Aa
 var main = new Vue({
     el: '#internships',
     data: {
-        ascending: false,
-        sortColumn: '',
-        rows: null
+        entries: []
     },
-
-    created: function () {
-        this.fetchData()
+    
+    mounted(){
+      this.fetchData();  
     },
-
-    watch: {
-        currentPage: 'fetchData'
-    },
-
+    
     methods: {
-
-        "fetchData": function () {
-            var xhr = new XMLHttpRequest()
-            var self = this
-            xhr.open('GET', sheet)
-            xhr.onload = function () {
-                self.entries = JSON.parse(xhr.responseText)
-                self.entries = self.entries.feed.entry
-                console.log(self.entry)
+        
+        fetchData: function(){
+            
+            var uwu = [];
+            
+             var url = "https://spreadsheets.google.com/feeds/list/1073N87suMlax63_94Jip5AaQxcq2Hxw5EnTJYMNKbg0/2/public/basic?alt=json";
+        
+        
+            
+        function parse(data){
+            
+            
+            
+            let temp = {
+                company: data.title.$t,
+                role: null,
+                pay: null,
+                notes: null
+            };
+            
+            let n = data.content.$t.split(',');
+            
+            for(let i of n){
+ 
+                let durr = i.split(":");
+                
+                switch(durr[0].replace(/ /g,'')){
+                    case "pay":
+                        temp.pay = durr[1]; break;
+                    case "role":
+                        temp.role = durr[1]; break;
+                    case "notes":
+                        temp.notes = durr[1]; break;
+                } 
             }
-            xhr.send()
-        },
+            
+            return temp;
+            
+        }
 
-        "sortTable": function sortTable(col) {
-            if (this.sortColumn === col) {
-                this.ascending = !this.ascending;
-            } else {
-                this.ascending = true;
-                this.sortColumn = col;
-            }
+        $.getJSON(url, function(res) {
 
-            var ascending = this.ascending;
-
-            this.rows.sort(function (a, b) {
-                if (a[col] > b[col]) {
-                    return ascending ? 1 : -1
-                } else if (a[col] < b[col]) {
-                    return ascending ? -1 : 1
-                }
-                return 0;
+               
             })
+            .done(function(res) {
+            
+                let test = [];
+            
+                for(let i of res.feed.entry){
+                   test.push(parse(i));
+                }
+            
+                uwu = test;
+            
+                /* this.entries =  [
+    {company: "COMPANY", pay: '305-917-1301', notes: 'IT Manager', industry: 'industry' },
+    {  company: "COMPANY", pay: '210-684-8953', notes: 'Paleontologist', industry: 'industry' }]; */
+            
+                console.log(test);
+            
+            });
+            
+            setTimeout(() => {
+            
+       
+            this.entries = uwu;
+            
+            }, 1000);
         }
-    },
-
-    computed: {
-        "columns": function columns() {
-
-            if (this.rows.length == 0) {
-                return [];
-            }
-            return Object.keys(this.rows[0])
-
-        }
+        
     }
 
 });
