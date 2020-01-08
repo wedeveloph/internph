9aabbee47ea7df604f639b12e203bf86759a7570
Chia <chiamisola@gmail.com>
2020-01-08 06:17:11 -0500

    filtering

--- main.js
+++ main.js
@@ -10,82 +10,160 @@ var main = new Vue({
     el: '#internships',
     data: {
         search: "",
-        by: "company",
+        sortByCompany: 0,
+        sortByPay: 0,
         reverse: false,
         entries: []
     },
-    
-    mounted(){
-      this.fetchData();
-       
+
+    mounted() {
+        this.fetchData();
+
     },
-    
+
     methods: {
-        
-        fetchData: function(){
-            
+
+        fetchData: function () {
+
             var self = this;
-            
-             var url = "https://spreadsheets.google.com/feeds/list/1073N87suMlax63_94Jip5AaQxcq2Hxw5EnTJYMNKbg0/2/public/basic?alt=json";
-        
-        
-            
-        function parse(data){
-            
-            
-            let temp = {
-                company: data.title.$t,
-                role: null,
-                pay: null,
-                notes: null
-            };
-            
-            
-            temp.pay = data.content.$t.split(',')[0].substr(4).trim().toLowerCase();
-            
-            temp.notes = (data.content.$t.split(',').slice(1).join(',')).substr(8);
-            
-            
-            return temp;
-            
-        }
 
-        $.getJSON(url, function(res) {
+            var url = "https://spreadsheets.google.com/feeds/list/1073N87suMlax63_94Jip5AaQxcq2Hxw5EnTJYMNKbg0/2/public/basic?alt=json";
 
-               
-            })
-            .done(function(res) {
-            
-                let test = [];
-            
-            
-                for(let i of res.feed.entry){
-                 test.push(parse(i));
-                }
-            
-            
-             document.getElementById("count").innerHTML = test.length;
-            
-            self.entries = test;
-            
-               // console.log(test);
-            
-            });
+
+
+            function parse(data) {
+
+
+                let temp = {
+                    company: data.title.$t,
+                    role: null,
+                    pay: null,
+                    notes: null
+                };
+
+
+                temp.pay = data.content.$t.split(',')[0].substr(4).trim().toLowerCase();
+
+                temp.notes = (data.content.$t.split(',').slice(1).join(',')).substr(8);
+
+
+                return temp;
+
+            }
+
+            $.getJSON(url, function (res) {
+
+
+                })
+                .done(function (res) {
+
+                    let test = [];
+
+
+                    for (let i of res.feed.entry) {
+                        test.push(parse(i));
+                    }
+
+                    document.getElementById("count").innerHTML = test.length;
+
+                    self.entries = test;
+
+                    // console.log(test);
+
+                });
         },
-        
-        sort: function(key){
-            
-    
+
+        sort: function (key) {
+
+            if (key == "company") {
+
+                if (this.sortByCompany == 0) {
+                    this.sortByCompany = 1;
+
+                    this.entries.sort((a, b) => {
+                        if (a.company < b.company) {
+                            return 1
+                        }
+
+                        if (a.company > b.company) {
+                            return -1
+                        }
+
+                        return 0
+                    })
+
+                } else {
+                    this.sortByCompany = 0
+
+                    this.entries.sort((a, b) => {
+                        if (a.company > b.company) {
+                            return 1
+                        }
+
+                        if (a.company < b.company) {
+                            return -1
+                        }
+
+                        return 0
+                    })
+
+                }
+
+            } else {
+
+                if (this.sortByPay == 0) {
+                    this.sortByPay = 1;
+
+                    this.entries.sort((a, b) => {
+                        if (a.pay < b.pay) {
+                            return 1
+                        }
+
+                        if (a.pay > b.pay) {
+                            return -1
+                        }
+
+                        return 0
+                    })
+
+                } else {
+                    this.sortByPay = 0;
+
+
+                    this.entries.sort((a, b) => {
+                        if (a.pay > b.pay) {
+                            return 1
+                        }
+
+                        if (a.pay < b.pay) {
+                            return -1
+                        }
+
+                        return 0
+                    })
+
+                }
+
+
+
+            }
+
+
+
         }
-        
+
     },
-    
+
     computed: {
-        
+
         query() {
-          return this.entries.filter(entry => {
-            return entry.company.toLowerCase().includes(this.search.toLowerCase())
-          })
+
+
+            return this.entries.filter(entry => {
+                return entry.company.toLowerCase().includes(this.search.toLowerCase())
+            })
+
+
         }
 
     }
